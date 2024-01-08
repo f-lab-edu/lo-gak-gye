c1296a827857f22c3e58a7a9e742c6df75708a7f
ber01 <ber01@naver.com>
2024-01-08 17:23:55 +0900

    feat: exceptionHandler

--- src/main/java/me/minkh/app/controller/AccountController.java
+++ src/main/java/me/minkh/app/controller/AccountController.java
@@ -1,16 +1,26 @@
 package me.minkh.app.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import me.minkh.app.exception.ErrorResponse;
 import me.minkh.app.domain.account.Account;
 import me.minkh.app.domain.account.AccountRepository;
 import me.minkh.app.dto.account.AccountRequestDto;
 import me.minkh.app.dto.account.AccountResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), e.getMessage());
+        return new ResponseEntity<>(errorResponse, status);
+    }
 
     private final AccountRepository accountRepository;
 
@@ -20,14 +30,13 @@ public class AccountController {
 
     @PostMapping
     public Account save(@RequestBody AccountRequestDto accountRequestDto) {
-        log.info("{}", accountRequestDto);
         return this.accountRepository.save(accountRequestDto.toEntity());
     }
 
     @GetMapping("/{id}")
     public AccountResponseDto findById(@PathVariable("id") Long id) {
-        Account account = this.accountRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(id + "가 없습니다."));
+        Account account = this.accountRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(id + "은 올바르지 않은 요청입니다."));
         return new AccountResponseDto(account);
     }
-
 }
---
+++ src/main/java/me/minkh/app/exception/ErrorResponse.java
@@ -0,0 +1,5 @@
+package me.minkh.app.exception;
+
+public record ErrorResponse(int status, String message) {
+
+}
