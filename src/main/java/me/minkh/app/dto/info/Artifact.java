7af6088663ee98a054aed625096dfe6ee575d0d2
ber01 <ber01@naver.com>
2024-01-12 22:27:01 +0900

    feat: Artifact, InfoResponseDto

---
+++ src/main/java/me/minkh/app/dto/info/Artifact.java
@@ -0,0 +1,11 @@
+package me.minkh.app.dto.info;
+
+public enum Artifact {
+
+    Nightmare, // 악몽
+    Salvation, // 구원
+    Dominion, // 지배
+    Entropy, // 사멸
+    Hallucination, // 환각
+
+}
---
+++ src/main/java/me/minkh/app/dto/info/InfoResponseDto.java
@@ -0,0 +1,17 @@
+package me.minkh.app.dto.info;
+
+import lombok.Getter;
+
+@Getter
+public class InfoResponseDto {
+
+      private Artifact artifact;
+
+      private int critical;
+
+      private int swiftness;
+
+      private int cursedDoll;
+
+      private int adrenaline;
+}
