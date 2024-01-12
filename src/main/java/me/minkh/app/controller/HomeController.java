da48a36bc15da8a5517f458189f310ad75dba7d3
ber01 <ber01@naver.com>
2024-01-12 22:28:12 +0900

    refactor: HomeController

--- src/main/java/me/minkh/app/controller/HomeController.java
+++ src/main/java/me/minkh/app/controller/HomeController.java
@@ -1,11 +1,16 @@
 package me.minkh.app.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import me.minkh.app.dto.info.InfoResponseDto;
+import me.minkh.app.dto.lostark.EngravingsDto;
+import me.minkh.app.dto.lostark.EquipmentDto;
 import me.minkh.app.dto.lostark.ProfileDto;
 import me.minkh.app.service.LostArkApiService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HomeController {
 
@@ -16,7 +21,13 @@ public class HomeController {
     }
 
     @GetMapping("/info/{characterName}")
-    public ProfileDto getProfiles(@PathVariable("characterName") String characterName) {
-        return this.lostArkApiService.getProfiles(characterName);
+    public InfoResponseDto getProfiles(@PathVariable("characterName") String characterName) {
+        EquipmentDto[] equipmentDto = this.lostArkApiService.getEquipment(characterName); // artifact 추출
+
+        ProfileDto profileDto = this.lostArkApiService.getProfiles(characterName); // 치명, 신속 추출
+
+        EngravingsDto engravings = this.lostArkApiService.getEngravings(characterName); // cursedDoll, adrenaline 추출
+
+        return null;
     }
 }
