74332dc9e0548f4b7064bb82190e99a2b61ada05
ber01 <ber01@naver.com>
2024-01-12 22:08:45 +0900

    feat: equipmentDto

---
+++ src/main/java/me/minkh/app/dto/lostark/EquipmentDto.java
@@ -0,0 +1,23 @@
+package me.minkh.app.dto.lostark;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.Getter;
+
+@Getter
+public class EquipmentDto {
+
+    @JsonAlias("Type")
+    private String type;
+
+    @JsonAlias("Name")
+    private String name;
+
+    @JsonAlias("Icon")
+    private String icon;
+
+    @JsonAlias("Grade")
+    private String grade;
+
+    @JsonAlias("Tooltip")
+    private String tooltip;
+}
