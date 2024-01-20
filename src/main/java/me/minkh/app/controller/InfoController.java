11bea4dc77048c362f3d038418f27da44d9973fc
ber01 <ber01@naver.com>
2024-01-20 22:25:16 +0900

    chore: 도커 컴포즈 이미지 빌드 체크

--- src/main/java/me/minkh/app/controller/InfoController.java
+++ src/main/java/me/minkh/app/controller/InfoController.java
@@ -1,13 +1,11 @@
 package me.minkh.app.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import me.minkh.app.dto.info.InfoResponse;
 import me.minkh.app.service.info.InfoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 public class InfoController {
 
