46fc9691177bf9d2a598723dfb50be63f8224e0c
ber01 <ber01@naver.com>
2024-01-20 22:32:25 +0900

    chore: build1

---
+++ src/main/java/me/minkh/app/controller/DockerController.java
@@ -0,0 +1,13 @@
+package me.minkh.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @GetMapping("/build")
+    public String build() {
+        return "build success 1";
+    }
+}
