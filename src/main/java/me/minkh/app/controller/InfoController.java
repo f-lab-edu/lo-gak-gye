81f8dbd0a6c7aef9a041f6a453c8abb555a4b12f
ber01 <ber01@naver.com>
2024-02-18 19:56:01 +0900

    feat: RateLimit 기능 추가

--- src/main/java/me/minkh/app/controller/InfoController.java
+++ src/main/java/me/minkh/app/controller/InfoController.java
@@ -1,8 +1,11 @@
 package me.minkh.app.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.minkh.app.config.auth.CurrentId;
 import me.minkh.app.dto.info.InfoResponse;
+import me.minkh.app.exception.TooManyRequestsException;
+import me.minkh.app.service.RateLimitService;
 import me.minkh.app.service.info.InfoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,19 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class InfoController {
 
     private final InfoService infoService;
-
-    public InfoController(InfoService homeService) {
-        this.infoService = homeService;
-    }
+    private final RateLimitService rateLimitService;
 
     @GetMapping("/info/{characterName}")
     public InfoResponse info(
             @PathVariable("characterName") String characterName,
             @CurrentId Long accountId
     ) {
+        if (rateLimitService.isLimit(accountId.toString())) {
+            throw new TooManyRequestsException("너무 많은 요청을 보냈습니다. 잠시 대기해 주세요.");
+        }
         return this.infoService.info(characterName, accountId);
     }
 }
